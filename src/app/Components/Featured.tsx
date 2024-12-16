509e4883a3339008e1d5afbed12ce79ccfa57649
harisaltafb105 <haris_memon1@yahoo.com>
2024-12-17 01:28:00 +0500

    Second Commit

--- src/app/Components/Featured.tsx
+++ src/app/Components/Featured.tsx
@@ -9,7 +9,7 @@ export const Featured = () => {
 <div className='sm:mt-[200px] md:mt-[260px] lg:mt-[330px] sm:ml-[100px] ml-[202px] max-sm:ml-[30px]'>{/*left side*/}
 <h3 className='text-[35px] max-sm:text-[25px] md:text-[40px] xl:text-[54px] lg:text-[42px]  font-medium'>Rocket Single Seater</h3>
 <button className='mt-7 lg:mt-11 max-sm:text-[18px] sm:text-base lg:text-[25px] text-2xl font-medium'> Shop Now </button>
-<Image src="/Line110.png" alt="img" height={0} width={121}></Image>
+<Image className="max-sm:w-[100px]" src="/Line110.png" alt="img" height={0} width={121}></Image>
 </div>
 
 
