cac587cd87fe613012d8dd85eef476df6d35e7a6
harisaltafb105 <haris_memon1@yahoo.com>
2025-01-21 03:06:07 +0500

    Another Commit

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,39 +1,3 @@
-// import React from "react";
-// import ProductList from "../Components/NewProductList";
-// import client from "../../../sanityClient";
-// import { Navbar2 } from "../Components/Navbar2";
-// import { ShopHeader } from "../Components/ShopHeader";
-// import { ShopNav2 } from "../Components/ShopNav2";
-// import { RouteFooter } from "../Components/RouteFooter";
-// import { Footer2 } from "../Components/Footer2";
-
-// export default async function product() {
-//   // Fetch data server-side
-//   const products = await client.fetch(`
-//     *[_type == 'product']{
-//       _id,
-//       name,
-//       imagePath,
-//       description,
-//       price,
-//       category,
-//       stockLevel,
-//       isFeaturedProduct
-//     }
-//   `);
-
-//   return (
-//     <div>
-//     <Navbar2 />
-//     <ShopHeader />
-//     <ShopNav2 />
-//     <ProductList products={products} />;
-//     <RouteFooter />
-//     <Footer2 />
-    
-
-// </div>
-//   )}
 import React from "react";
 import ProductListPage from "../Components/NewProductList";
 import client from "../../../sanityClient";
